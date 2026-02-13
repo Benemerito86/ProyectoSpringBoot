397c4ed4b7d9ecc67eda6a7775043ad1f660303d
Miguel Benjumea <benemerito.benjumea@gmail.com>
2026-02-13 19:09:27 +0100

    Delete saas-core directory

--- saas-core/src/main/java/com/tuorg/saas_core/model/SubscriptionStatus.java
+++
@@ -1,7 +0,0 @@
-package com.tuorg.saas_core.model;
-
-public enum SubscriptionStatus {
-    ACTIVE, // ACTIVA
-    CANCELLED, // CANCELADA
-    OVERDUE // MOROSA
-}
--- saas-core/src/main/java/com/tuorg/saas_core/repository/PlanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tuorg.saas_core.repository;
-
-import com.tuorg.saas_core.model.Plan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlanRepository extends JpaRepository<Plan, Long> {
-}
